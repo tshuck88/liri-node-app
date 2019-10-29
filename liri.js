19b13f78f289f12c974a5e15db74d2318bf97bad
Tyler Shuck <tshuck88@gmail.com>
2019-10-29 15:44:09 -0700

    created a function to search for songs with spotify api

--- liri.js
+++ liri.js
@@ -1,6 +1,34 @@
 require("dotenv").config();
 const keys = require("./key.js");
 const axios = require('axios');
-const spotify = require('node-spotify-api');
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 const moment = require('moment');
 moment().format();
+
+const command = process.argv[2].toLowerCase();
+const searchInput = process.argv.slice(2).join(" ");
+const divider = "============================="
+
+function songSearch(song) {
+    spotify.search({ type: 'track', query: song, limit: "1" }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        const artistName = [];
+        const returnedArtists = data.tracks.items[0].artists;
+
+        //console.log(JSON.stringify(data, null, 2)); 
+        for (let i = 0; i < returnedArtists.length; i++) {
+             artistName.push(returnedArtists[i].name);
+        }
+        console.log(divider);
+        console.log("Artist(s): " + artistName.join(", "));
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("Preview URL: " + data.tracks.items[0].preview_url);
+        console.log(divider);
+    });
+}
+
+songSearch(searchInput)
