c41119b4037876f4bdc21d1711f1eb60016b2213
Tyler Shuck <tshuck88@gmail.com>
2019-10-29 13:34:28 -0700

    created initial files and installed dependecies

---
+++ liri.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const keys = require("./key.js");
+const axios = require('axios');
+const spotify = require('node-spotify-api');
+const moment = require('moment');
+moment().format();
