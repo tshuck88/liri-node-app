65badb71af90bbb18cde55f492b0dcb28f59b9c0
Tyler Shuck <tshuck88@gmail.com>
2019-10-30 11:31:10 -0700

    created a movie search function with OMDB API

--- liri.js
+++ liri.js
@@ -64,4 +64,35 @@ function concertSearch(artist) {
         });
 }
 
-concertSearch(searchInput)
+function movieSearch(movie) {
+    const queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie
+    axios.get(queryURL).then(
+        function (response) {
+            console.log(divider);
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Plot: " + response.data.Plot);
+            console.log(divider);
+        }).catch(function (error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+movieSearch(searchInput)
