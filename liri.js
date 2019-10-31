444416892ad4a72c9a9661f814aa471219a4acd5
Tyler Shuck <tshuck88@gmail.com>
2019-10-30 23:43:46 -0700

    created a logText function to to log outputs to the console and to the log.txt file

--- liri.js
+++ liri.js
@@ -12,6 +12,25 @@ if (process.argv[2] !== undefined) {
 }
 let searchInput = process.argv.slice(3).join(" ");
 const divider = "=============================";
+const text = process.argv.slice(2).join(" ") + "\n";
+
+fs.appendFileSync("log.txt", text, function (err) {
+    if (err) {
+        logText(err);
+    } else {
+        logText("Content logged to log.txt");
+    }
+});
+
+function logText(message) {
+    console.log(message)
+    fs.appendFileSync("log.txt", message + "\n", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
 
 readCommand(userCommand);
 
@@ -30,19 +49,20 @@ function readCommand(command) {
             doWhatItSays();
             break;
         default:
-            console.log('Not a recognized command. Try "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says".');
+            logText(divider);
+            logText('Not a recognized command. Try "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says".');
+            logText(divider);
     }
 }
-
 
 function songSearch(song) {
     if (song === "") {
         song = "The Sign Ace of Base";
-        console.log("No song was entered. Check out 'The Sign' by Ace of Base.");
+        logText("No song was entered. Check out 'The Sign' by Ace of Base.");
     }
     spotify.search({ type: 'track', query: song, limit: "5" }, function (err, data) {
         if (err) {
-            return console.log('Error occurred: ' + err);
+            return logText('Error occurred: ' + err);
         }
         for (let i = 0; i < data.tracks.items.length; i++) {
             const artistName = [];
@@ -50,12 +70,12 @@ function songSearch(song) {
             for (let j = 0; j < returnedArtists.length; j++) {
                 artistName.push(returnedArtists[j].name);
             }
-            console.log(divider);
-            console.log("Artist(s): " + artistName.join(", "));
-            console.log("Song: " + data.tracks.items[i].name);
-            console.log("Album: " + data.tracks.items[i].album.name);
-            console.log("Preview URL: " + data.tracks.items[i].preview_url);
-            console.log(divider);
+            logText(divider);
+            logText("Artist(s): " + artistName.join(", "));
+            logText("Song: " + data.tracks.items[i].name);
+            logText("Album: " + data.tracks.items[i].album.name);
+            logText("Preview URL: " + data.tracks.items[i].preview_url);
+            logText(divider);
         }
     });
 }
@@ -63,85 +83,86 @@ function songSearch(song) {
 function concertSearch(artist) {
     if (artist === "") {
         artist = "Diplo";
-        console.log("No artist was entered. Here are Diplo's upcoming shows.");
+        logText("No artist was entered. Here are Diplo's upcoming shows.");
     }
     const queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
     axios.get(queryURL).then(
         function (response) {
             for (let i = 0; i < response.data.length; i++) {
                 const venue = response.data[i].venue
-                console.log(divider);
-                console.log(venue.name);
+                logText(divider);
+                logText(venue.name);
                 if (venue.region !== "") {
-                    console.log(venue.city + ", " + venue.region);
-                    console.log(venue.country);
+                    logText(venue.city + ", " + venue.region);
+                    logText(venue.country);
                 } else {
-                    console.log(venue.city + ", " + venue.country);
+                    logText(venue.city + ", " + venue.country);
                 }
-                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                console.log(divider);
+                logText(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                logText(divider);
             }
         }).catch(function (error) {
             if (error.response) {
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
+                logText("---------------Data---------------");
+                logText(error.response.data);
+                logText("---------------Status---------------");
+                logText(error.response.status);
+                logText("---------------Status---------------");
+                logText(error.response.headers);
             } else if (error.request) {
-                console.log(error.request);
+                logText(error.request);
             } else {
-                console.log("Error", error.message);
+                logText("Error", error.message);
             }
-            console.log(error.config);
+            logText(error.config);
         });
 }
 
 function movieSearch(movie) {
     if (movie === "") {
         movie = "Mr. Nobody";
-        console.log("No movie was entered. Here are some details about the movie 'Mr. Nobody'.");
+        logText("No movie was entered. Here are some details about the movie 'Mr. Nobody'.");
     }
     const queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie
     axios.get(queryURL).then(
         function (response) {
-            console.log(divider);
-            console.log("Title: " + response.data.Title);
-            console.log("Year: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Actors: " + response.data.Actors);
-            console.log("Plot: " + response.data.Plot);
-            console.log(divider);
+            logText(divider);
+            logText("Title: " + response.data.Title);
+            logText("Year: " + response.data.Year);
+            logText("IMDB Rating: " + response.data.imdbRating);
+            logText("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            logText("Country: " + response.data.Country);
+            logText("Language: " + response.data.Language);
+            logText("Actors: " + response.data.Actors);
+            logText("Plot: " + response.data.Plot);
+            logText(divider);
         }).catch(function (error) {
             if (error.response) {
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
+                logText("---------------Data---------------");
+                logText(error.response.data);
+                logText("---------------Status---------------");
+                logText(error.response.status);
+                logText("---------------Status---------------");
+                logText(error.response.headers);
             } else if (error.request) {
-                console.log(error.request);
+                logText(error.request);
             } else {
-                console.log("Error", error.message);
+                logText("Error", error.message);
             }
-            console.log(error.config);
+            logText(error.config);
         });
 }
 
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-            console.log(error);
+            logText(error);
         }
         const dataArray = data.toString().split("\r\n").join(",").split(",")
-        console.log(dataArray)
+        logText(dataArray)
         const dataCommand = dataArray[0];
         searchInput = dataArray[1];
         readCommand(dataCommand);
     });
 }
+
